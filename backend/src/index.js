c9c0678283f893e2c6b8fe035d047845af415188
Rupam-Mondal <rupammondal564@gmail.com>
2026-09-03 12:13:48 +0530

    Inngest setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
+import dotenv from 'dotenv/config';
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 
