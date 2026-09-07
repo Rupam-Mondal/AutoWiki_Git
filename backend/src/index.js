11756d8c702eef023862e8fc477b248fc4f39a24
Rupam-Mondal <rupammondal564@gmail.com>
2026-09-07 08:26:52 +0530

    major functionality is done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import indexRoutes from "./routes/index.routes.js"
+import chatRoutes from "./routes/chat.routes.js"
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/index" , indexRoutes);
+app.use("/api/chat", chatRoutes);
 
 
 app.listen(3000 , () => {
--- backend/src/inngest/index.js
+++ backend/src/inngest/index.js
@@ -1,6 +1,7 @@
 import { inngest } from "./client.js";
+import { askQuestionFn } from "./functions/askQuestions.js";
 import { helloWorld } from "./functions/helloWorld.js";
 import { indexRepo } from "./functions/indexRepo.js";
 
 export { inngest };
-export const functions = [helloWorld , indexRepo];
+export const functions = [helloWorld , indexRepo , askQuestionFn];
